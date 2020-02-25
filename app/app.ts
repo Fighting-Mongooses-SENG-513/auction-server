28e33526956154c293a851671606773a27a84434
James <jbews11@gmail.com>
2020-02-25 10:46:09 -0700

    - Create user api

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,11 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+import mongoose from 'mongoose';
 
 class App {
     public app: express.Application;
     public port: number;
+    public mongoConnection: string = 'mongodb+srv://fightingMongoose:02hGHEQ3WbO1mjIV@cluster0-wrbvu.mongodb.net/test?retryWrites=true&w=majority';
 
     constructor(routes: any[], port: number) {
         this.app = express();
@@ -11,6 +13,7 @@ class App {
 
         this.initMiddleware();
         this.initRoutes(routes);
+        this.initDB();
     }
 
     private initMiddleware() {
@@ -20,6 +23,20 @@ class App {
     private initRoutes(routes: any[]) {
         routes.forEach(route => {
             this.app.use('/', route.router);
+        });
+    }
+
+    private initDB() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoConnection, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log('Mongo connection suceeded.');
+        })
+        .catch(() => {
+            console.log('Mongo connection failed.');
         });
     }
 
